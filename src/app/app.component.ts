c16fc1c51f1f94512ffc443ae7f838ac2a725b91
Agustin Aleksandroff <agustinaleks@gmail.com>
2023-07-12 18:09:55 -0300

    trabajo practico finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
+
+interface Producto {
+  nombre: string;
+  costo: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+
+  
+
 })
 export class AppComponent {
   title = 'practica_angular';
+
+  productos: Producto[] = [
+    { nombre: 'Producto A',
+      costo: '$1000'
+     },
+    { nombre: 'Producto B',
+      costo: '$2000'
+    },
+    { nombre: 'Producto C',
+      costo: '$3000'
+    },
+    { nombre: 'Producto D',
+      costo: '$4000'
+     }
+  ];
+
+  
+  mostrarLista = true;
+
+  lista() {
+    this.mostrarLista = !this.mostrarLista;
+  }
+
+  
 }
